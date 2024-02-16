000e0be9e0f6061296d224e067a184e4bac11fa9
Utkarsh Inamdar <utk.knight10@gmail.com>
2024-02-16 14:15:23 +0530

    Setup 2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_requirements(file_path:str)->List[str]:
     '''
     
     requirements = []
-    with open("requirements.txt") as file_obj:
+    with open(file_path) as file_obj:
         requirements = file_obj.readlines()
         requirements = [req.replace("\n", " ") for req in requirements]
         
